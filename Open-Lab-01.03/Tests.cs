34e8fe94e95b4953f6e2125524b6db2c98d8fd27
leonardmoravec <leonardmoravec154@gmail.com>
2021-10-13 08:51:06 +0200

    increase the number by 1

--- Open-Lab-01.03/Tests.cs
+++ Open-Lab-01.03/Tests.cs
@@ -9,6 +9,7 @@ namespace Open_Lab_01._03
         public void Addition([Range(-10, 10)] int number)
         {
             var exercise = new Exercise();
+            
             Assert.That(exercise.Addition(number), Is.EqualTo(number + 1));
         }
     }
